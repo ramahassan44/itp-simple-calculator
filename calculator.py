eb7a295c1de62cfd1cb154b71c34d768393c11af
ramahassan44 <156920833+ramahassan44@users.noreply.github.com>
2024-07-07 00:34:22 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,35 @@
+import math
 def add(x, y):
     return x + y
 
 
 def subtract(x, y):
-    pass
+    return x-y
 
 
 def divide(x, y):
-    pass
+    if y == 0:
+        print("Cannot divide by zero!")
+    return x / y
+
 
 
 def multiply(x, y):
-    pass
+    x*y
 
 
 def square(x):
-    pass
+    return x**2
 
 
 def power(x, y):
-    pass
+    return x**y
 
 
 def sqrt(x):
-    pass
+    if x < 0:
+       print("Square root of negative number is not real")
+    else:    
+    return math.sqrt(x)
+    
+
